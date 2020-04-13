58ddc58e14ddf8b37e2cb2f6512dadcc9bb95300
Dgonseg <gose1992@gmail.com>
2020-04-13 11:57:17 +0200

    1 file modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { SelectComponent } from './components/form/select/selectComponent'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SelectComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

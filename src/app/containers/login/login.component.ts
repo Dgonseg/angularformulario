8a1a6fce755d662cc2fd384c0a6b0e45e057b69a
Dgonseg <gose1992@gmail.com>
2020-04-19 12:55:21 +0200

    1 file modified, 4 added, 3 deleted

--- src/app/containers/login/login.component.ts
+++ src/app/containers/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "../../services/auth.services";
+import { AuthService } from "../../shared/services/auth.services";
 
 
 @Component({

6d179d15900e8a7f1ccd9035146ef7205fb9c01f
Dgonseg <gose1992@gmail.com>
2020-04-22 23:32:09 +0200

    3 files modified

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
+  profile: object;
+
   constructor() { }
 
   ngOnInit() {
+    // this.profile
   }
 
 }

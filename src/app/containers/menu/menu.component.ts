ebcd3d26014ff801c77514ff6e08a12aa339606d
Dgonseg <gose1992@gmail.com>
2020-04-27 17:40:24 +0200

    2 files modified

--- src/app/containers/menu/menu.component.ts
+++ src/app/containers/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.services";
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
